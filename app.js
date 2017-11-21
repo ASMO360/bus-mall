eab35a7c9e12be8992213e46dc63ba9125ec46ca
ASMO360 <uafredrickson@gmail.com>
2017-11-20 18:17:52 -0800

    update

--- app.js
+++ app.js
@@ -2,18 +2,32 @@
 
 var allProducts = []; //this is where the objects are stored
 var productNames = []; // this will create my results list
+var allImages = []; //this will create an array of images
 
-function Product(name, path) {
+function Product(name, path, id) {
   this.name = name;
-  this.path = path;
+  this.path = "img/" + path;
   this.totalClick = 0;
   this.totalDisplay = 0;
+  allImages.push(this.path);
   allProducts.push(this);
   productNames.push(this.name);
 }
 
+
+var allImages = [];
+
+new PageImage('item', 'item.jpg', 'id',allImages);
+
+function PageImage('itemName', 'Path', 'id', allImages) {
+
+}
+
+
 // TODO: Don't forget to build your objects. How can you do this withough having to write 14 lines of `new Product(., ., .)`?
-var bag = new Product('bag', 'img/bag.jpg' );
+var bag = new Product('bag', 'bag.jpg' );
+console.log(allProducts);
+var banana = new Product('banana', 'banana.jpg');
 console.log(allProducts);
 
 var productRank = {
@@ -25,7 +39,9 @@ var productRank = {
   },
 
   displayImages: function() {
-
+    var myImage = new Image(200,200);
+    myImage.src = this.path;
+    document.getByElementId
   },
 
   tallyClicks: function(elementId) {
