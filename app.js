d1d9162ffa3b8815a3ead07bbb1bd80ee03a9f46
ASMO360 <uafredrickson@gmail.com>
2017-11-20 16:46:34 -0800

    update

--- app.js
+++ app.js
@@ -7,26 +7,29 @@ function Product(name, path) {
   this.name = name;
   this.path = path;
   this.totalClick = 0;
+  this.totalDisplay = 0;
   allProducts.push(this);
-  productNames.push(this.name)
+  productNames.push(this.name);
 }
 
 // TODO: Don't forget to build your objects. How can you do this withough having to write 14 lines of `new Product(., ., .)`?
+var bag = new Product('bag', 'img/bag.jpg' );
+console.log(allProducts);
 
 var productRank = {
   // TODO: All the properties of the object! What do you think you need? Try to write one piece at a time and make sure it does what you want before writing a little more.
   // NOTE: A-C-P reminder... Make very intentional and iterative changes to your code, and then A-C-P.
 
   getRandomIndex: function() {
-    return (math.random() * ((parseInt(allProducts.length) - 1) +1)
+    return Math.floor(Math.random() * (parseInt(allProducts.length) - 1) + 1);
   },
 
   displayImages: function() {
-    // TODO: Hmm... what's going to happen here?
+
   },
 
   tallyClicks: function(elementId) {
-    // TODO: Hmm... what's going to happen here?
+    this.totalClick +=
   },
 
   displayResults: function() {
