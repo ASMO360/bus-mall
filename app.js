1058af921c6822c353aea93dbd8d46a0d09ba90a
ASMO360 <uafredrickson@gmail.com>
2017-11-22 16:57:37 -0800

    I have a graphgit add .!

--- app.js
+++ app.js
@@ -66,20 +66,86 @@ var tracker = {
 
   onClick: function(event) {
     console.log(event.target.id);
-    if (tracker.clickcount === 3) { //Change this to 24
+    if (tracker.clickcount === 15) { //Change this to 24
       tracker.imagesEl.removeEventListener('click', tracker.onClick);
 
-      var ulEl = document.createElement('ul');
-      tracker.resultsEl.appendChild(ulEl);
-      console.log('ulEl: ',ulEl);
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero:true
+              }
+            }]
+          }
+        }
+      });
 
-      for(var j = 0; j < allProducts.length; j++) {
-        var liEl = document.createElement('li');
-        console.log(liEl);
-        liEl.textContent = ('product: ' + allProducts[j].name + ' votes: ' + allProducts[j].votes);
-        ulEl.appendChild(liEl);
+      for(var h = 0; h < allProducts.length; h++) {
+        myChart.data.datasets[0].data[h] = allProducts[h].votes;
+        myChart.data.labels[h] = allProducts[h].name;
       }
-      console.log('it passed the for loop');
+
+
+
+
+
+
+
 
     } else if (event.target.id === 'images') {
       console.log('no image clicked');
